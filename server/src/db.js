7a905fbcbe25aec8cb4eabf8004d510e9db01494
Will G <evelandy@gmail.com>
2023-08-19 00:46:19 -0500

    Save progress

--- server/src/db.js
+++ server/src/db.js
@@ -2,6 +2,11 @@
 temporary DB for this project
 */
 export let users = [
-  { id: 1, name: "John Doe", email: "john@gmail.com", age: 22 },
-  { id: 2, name: "Jane Doe", email: "jane@gmail.com", age: 23 }
+  { id: 4, userID: "0001", name: "John Smith", title: "SDM", admin: true },
+  { id: 7, userID: "0003", name: "Jeff James", title: "Product Owner", admin: true },
+  { id: 12, userID: "0007", name: "Wade Wilson", title: "Project Manager", admin: true },
+  { id: 32, userID: "0016", name: "Scott Summers", title: "Account Manager", admin: true },
+  { id: 22, userID: "0018", name: "Remmy LeBeau", title: "Developer I", admin: false },
+  { id: 39, userID: "0021", name: "Kitty Parade", title: "Developer II", admin: false },
+  { id: 52, userID: "0084", name: "Max Million",  title: "Designer", admin: false }
 ];
